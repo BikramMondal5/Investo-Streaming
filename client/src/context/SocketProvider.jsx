a2dbeca0814a3a862475b749bcc40f427d12a4a5
Bikram Mondal <codesnippets45@gmail.com>
2025-10-13 19:28:03 +0530

    Backend

--- client/src/context/SocketProvider.jsx
+++ client/src/context/SocketProvider.jsx
@@ -9,7 +9,7 @@ export const useSocket = () => {
 };
 
 export const SocketProvider = (props) => {
-  const socket = useMemo(() => io("localhost:8000"), []);
+  const socket = useMemo(() => io("https://investo-streaming.onrender.com"), []);
 
   return (
     <SocketContext.Provider value={socket}>
